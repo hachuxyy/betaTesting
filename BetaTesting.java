286fd88b06e07e0762a0a91f2e7f2381b922c3ef
hachuxyy <msamsularifinb6@gail.com>
2025-09-08 20:56:05 +0700

    Penambahan File Baru Java Haqi

--- BetaTesting.java
+++ BetaTesting.java
@@ -1,5 +1,5 @@
 public class BetaTesting {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Halo Nama aya M. Samsul Arifin Baihaqi");
     }
 }
